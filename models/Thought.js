a5d158fd5cd19b690899eaa3aa801b624d79a221
Martin-Borchert <108767551+ConnerMart@users.noreply.github.com>
2022-10-01 11:11:30 -0600

    set up first part of user routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
+const { ObjectId } = require("bson");
+
+// TODO: use getter for createdAt timestamp
+
+// TODO: finish reaction schema
+const reactionSchema = new mongoose.Schema({
+  // TODO reactionBody 280 character maximum
+  reactionId: { type: ObjectId, default: new ObjectId() },
+  reactionBody: { type: String, required: true },
+  username: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
 const thoughtSchema = new mongoose.Schema({
   // TODO: fill this out according to instructions
   // TODO thoughtText between 1-280 characters
-  // TODO createdAt Date, default to current timestamp, use getter to format on query
-  // TODO reactions reference
   // TODO virtual called reactionCount
   thoughtText: { type: String, required: true },
-  createdAt: {},
+  createdAt: { type: Date, default: Date.now },
   username: { type: String, required: true },
-  reactions: [],
-  // TODO: reaction schema inside above ??
+  reactions: [reactionSchema],
 });
 
 const Thought = mongoose.model("Thought", thoughtSchema);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,31 +1,26 @@
 const router = require("express").Router();
 
-// path to /
-// GET all users
-// GET a single user by its _id and populated thought and friend data
-// POST a new user
-// PUT to update a user by its _id
-// DELETE to remote a user by its _id
+const {
+  getUsers,
+  getOneUser,
+  postNewUser,
+  updateUser,
+  deleteUser,
+} = require("../../controllers/userController");
 
-// path to /:userId/friends/:friendId
-// POST to add a new friend to user's friend list
-// DELETE to remove a friend from a user's friend list
+// TODO: update these routes after finalizing models
+
+router.route("/").get(getUsers).post(postNewUser);
+// GET all users
+// POST a new user
+
+router.route("/:userId").get(getOneUser).put(updateUser).delete(deleteUser);
+// GET a single user by its _id and populated thought and friend data
+// PUT to update a user by its _id
+
+// DELETE to remove a user by its _id
+
+router.route("/:userId/friends/:friendId");
+// TODO: the rest of these routes
 
 module.exports = router;
-
-// TODO: recreate below with methods from userController:
-
-// from class activities:
-// const {
-//   getUsers,
-//   getSingleUser,
-//   createUser,
-// } = require('../../controllers/userController');
-
-// TODO: recreate below with methods above, matched up to routes on lines 3, 10
-
-// // /api/users
-// router.route('/').get(getUsers).post(createUser);
-
-// // /api/users/:userId
-// router.route('/:userId').get(getSingleUser);
