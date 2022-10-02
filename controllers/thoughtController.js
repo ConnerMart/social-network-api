c010c097cd65ffb7d20ba4af53e139a27bc95f58
Martin-Borchert <108767551+ConnerMart@users.noreply.github.com>
2022-10-02 10:46:32 -0600

    set up remaining routes/controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,76 @@
 const Thought = require("../models/Thought");
+
+module.exports = {
+  // functions here
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  getOneThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  postNewThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.status(500).json(err));
+    // TODO: (don't forget to push the new thought's _id to the associated user's thoughts array field)
+  },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this ID" })
+          : res.json(thought)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) => {
+        !thought
+          ? res.status(404).json({ message: "No thought with this ID" })
+          : res.json({ message: "Thought has been deleted" });
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  postNewReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this ID" })
+          : res.json({ message: "Reaction has been deleted" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,14 +1,35 @@
 const router = require("express").Router();
 
+const {
+  // functions here
+  getThoughts,
+  getOneThought,
+  postNewThought,
+  updateThought,
+  deleteThought,
+  postNewReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
 // path to /
+router.route("/").get(getThoughts).post(postNewThought);
 // GET to get all thoughts
+// POST to create a new thought
+
+router
+  .route("/:thoughtId")
+  .get(getOneThought)
+  .put(updateThought)
+  .delete(deleteThought);
 // GET to get a single thought by its _id
-// POST to create a new thought (don't forget to push the new thought's _id to the associated user's thoughts array field)
 // PUT to update a thought by its _id
 // DELETE to remove a thought by its _id
 
 // path to /:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(postNewReaction);
 // POST to create a reaction stored in a single thought's reactions array field
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 // DELETE to pull and remove a reaction by the reaction's reactionId value
 
 module.exports = router;
